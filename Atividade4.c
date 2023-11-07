b89e02f116c20d47d58a569c14c9a41277c166b1
Everson <143636906+Everson777@users.noreply.github.com>
2023-11-07 16:00:27 -0300

    Add files via upload

---
+++ Atividade4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	
+int idade;	
+	
+//Pedindo pro usuário digitar sua idade
+printf("Digite sua idade:");
+scanf("%d",&idade);	
+	
+//Verificando informações
+
+if(idade >= 18){
+	
+	printf("Acesso permitido!");
+
+}else{
+	
+	printf("Acesso negado!");
+}
+
+
+
+
+}
